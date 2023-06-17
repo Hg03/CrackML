e56476509fbf95981fd7182d648383735f73ed86
Harish Gehlot <69637720+Hg03@users.noreply.github.com>
2023-06-17 23:40:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
 import streamlit as st
 from markdownlit import mdlit
 
+st.set_page_config(layout='wide')
+
 mdlit('# [blue]Cracking[/blue] the [yellow]Machine Learning[/yellow] Interview 🔥')
+
+st.sidebar.title('Why you are here')
+with st.form('My form'):
+    email_id = st.sidebar.text_input('Your Email ID')
+    submit = st.sidebar.button('Submit')
+
+mdlit('### Q1. Define [blue]Machine Learning[/blue] and How it is different from [blue]Artificial Intelligence[/blue] ?')
+st.markdown('> ### Machine Learning is a subset of Artificial Intelligence that aims at making systems learn automatically from the data provided and improve their learnings over time without being explicitly programmed. Artificial Intelligence (AI) is the broader concept of machines being able to carry out tasks in a way that could be considered as smart. The machines not necessarily learn from the data but may exhibit intelligence in performing certain tasks that mimic the characteristic of human intelligence.')
